1be03e4e1e35bb60711ca6d7eeed47ea6dcba813
Nan Santos <pixelado405@gmail.com>
2026-04-27 20:14:10 -0300

    Trabalhando com Arrays!

--- 06_array.php
+++ 06_array.php
@@ -13,7 +13,52 @@ $carroDois = [
 ];
 
 echo $carroDois['modelo'];
+echo ' - ';
 echo $carroDois['marca'];
+echo ' - ';
 echo $carroDois['cor'];
+
+$carroTres = [
+    'modelo' => 'Punto',
+    'marca' => 'Fiat',
+    'cor' => 'Amarelo',
+];
+
+echo '<br>';
+
+echo $carroTres['modelo'];
+echo ' - ';
+echo $carroTres['marca'];
+echo ' - ';
+echo $carroTres['cor'];
+
+echo '<br>';
+
+$carroQuatro = [
+    'modelo' => 'Kwid',
+    'marca' => 'Renault',
+    'cor' => 'Branco',
+];
+
+echo $carroQuatro['modelo'];
+echo $carroQuatro['marca'];
+echo $carroQuatro['cor'];
+
+echo '<br><br>';
+
+$carroCinco = [
+    'modelo' => 'Brasília',
+    'marca' => 'Volkswagen',
+    'cor' => 'Azul',
+];
+
+echo 'Marca: ';
+echo $carroCinco['marca'];
+echo '<br>';
+echo 'Modelo: ';
+echo $carroCinco['modelo'];
+echo '<br>';
+echo 'Cor: ';
+echo $carroCinco['cor'];
 
 ?>
