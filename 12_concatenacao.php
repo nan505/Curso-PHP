d255956033939b8bff89291e06536db157f444e9
Nan Santos <pixelado405@gmail.com>
2026-05-04 19:48:16 -0300

    Aprendendo sobre concatenação!

---
+++ 12_concatenacao.php
@@ -0,0 +1,20 @@
+<?php
+
+$var1 = 'João';
+$var2 = 'da Silva';
+
+//echo $var1 . ' ' . $var2 . ' ' . 12;
+
+$var3 = $var1 . ' ' . $var2 . ' ' . 12;
+//echo $var3;
+
+$nome = 'Rubens Pereira';
+$empresa = 'Senac Americana';
+
+//echo 'O ' . $nome . ' é fundador da empresa: ' . $empresa;
+echo "O {$nome} é fundador da empresa: {$empresa}";
+echo '<br>';
+
+//echo 'Aqui no ' . $empresa . ', fundado por ' . $nome .  ', prezamos por um ensino de qualidade.';
+
+?>
