c172bacdb2565a577fce40f3502577aefbe0ccec
Nan Santos <pixelado405@gmail.com>
2026-05-11 22:21:22 -0300

    Formulário para cadastro de adotantes criado!

---
+++ 02_dados_txt/cadastrar.php
@@ -0,0 +1,10 @@
+<?php
+
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$telefone = $_POST['telefone'];
+$cpf = $_POST['cpf'];
+
+echo "{$nome}, {$email}, {$telefone}, {$cpf}";
+
+?>
---
+++ 02_dados_txt/formulario.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro de Adotantes</title>
+</head>
+<body>
+    <h1>Cadastro de Adotantes</h1>
+    <form action="cadastrar.php" method="post">
+        <label>Nome completo:</label>
+        <input type="text" name="nome" required><br>
+        <label>E-mail:</label>
+        <input type="email" name="email" required><br>
+        <label>Número de telefone:</label>
+        <input type="tel" name="telefone" maxlength="11" required><br>
+        <label>CPF:</label>
+        <input type="text" name="cpf" maxlength="11" required><br>
+        <button type="submit">Salvar</button>
+        <button type="reset">Limpar</button>
+    </form>
+</body>
+</html>
